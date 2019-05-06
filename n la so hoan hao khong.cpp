7d3b478482a7bf9e7c9da4cad5aaae286920fefd
duyk30b <duyk30b@gmail.com>
2019-05-06 14:11:02 +0700

    update bai ve nha 3/5

--- n la so hoan hao khong.cpp
+++ n la so hoan hao khong.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	int n,s=0;
-	printf("Nhap 1 so bat ky n = ");
-	scanf("%d",&n);
-	for(int a=1;a<n;a++){
-		if(n%a==0){
-			s = s + a;
+	int m,n,s;
+	printf("Nhap 1 so bat ky m = ");
+	scanf("%d",&m);
+	for(n=1;n<m;n++){
+		s=0;
+		for(int a=1;a<n;a++){
+			if(n%a==0){
+				s = s + a;
+			}
 		}
-	}
-	if(s==n){
-		printf("n la so hoan hao");
-	}
-	else{
-		printf("Tong cac uoc cua n la s = %d -> n khong phai so hoan hao",s);
+		if(s==n){
+			printf("%d la so hoan hao\n",s);
+		}
 	}
 	return 0;
 }
--- so 100 trong day Fibonaci.cpp
+++ so 100 trong day Fibonaci.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main(){
+	//Dãy Fibonaci: 1,1,2,3,5,8,13,21,34,55,89
 	long long a=1,b=1;
-	for(int i=3;i<=100;i+=2){
+	int n;
+	do{
+		printf("Dien so can tim thu n >= 3 trong day Fibonaci, n = ");
+		scanf("%d",&n);
+	}
+	while(n < 3);
+	for(int i=3;i<=n;i+=2){
 		a = a + b;
 		b = b + a;
 	}
-	printf("So thu 99 va 100 trong day Fibonaci la %lld, %lld",a,b);
+	printf("So thu %d va %d trong day Fibonaci la %lld, %lld",(n-1)/2*2+1,(n-1)/2*2+2,a,b);
 }
