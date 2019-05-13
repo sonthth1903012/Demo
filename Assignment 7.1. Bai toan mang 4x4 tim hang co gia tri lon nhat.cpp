5036e4943063f949bd36547e4dbf164c252f27e9
duyk30b <duyk30b@gmail.com>
2019-05-13 14:01:55 +0700

    Assignment 7

--- Assignment 7.1. Bai toan mang 4x4 tim hang co gia tri lon nhat.cpp
+++ Assignment 7.1. Bai toan mang 4x4 tim hang co gia tri lon nhat.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int array[4][4];
+	int array[4][4],s[4]={0,0,0,0};
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
 			printf("Nhap gia tri cua array[%d][%d] = ",i,j);
 			scanf("%d",&array[i][j]);
+			s[i] = s[i] + array[i][j];
 		}
 	}
 	printf("Gia tri cua mang la: \n");
@@ -15,23 +16,17 @@ int main(){
 		}
 	printf("\n");
 	}
-	int s[4]={0,0,0,0};
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			s[i] = s[i] + array[i][j];
-		}
-	}
 	printf("Gia tri cua mang s la: ");
 	for(int i=0;i<4;i++){
 		printf("%5d",s[i]);
 	}
-	int a = s[0],b;
+	int max = s[0],vitri;
 	for(int i=1;i<4;i++){
-		if(a<s[i]){
-			a = s[i];
-			b = i;
+		if(max<s[i]){
+			max = s[i];
+			vitri = i;
 		}
 	}
-	printf("\nHang co tong gia tri lon nhat = %d o hang chi so %d",a,b);
+	printf("\nHang co tong gia tri lon nhat = %d o hang chi so %d",max,vitri);
 	return 0;
 }
